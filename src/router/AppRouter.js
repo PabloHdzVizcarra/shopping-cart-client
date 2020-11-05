b63bad0d700380ded3c90d9baa5121e07af3be9d
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-11-05 11:45:18 -0600

    Feat: Added auth with token

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -5,37 +5,50 @@ import {
   Switch
 } from 'react-router-dom'
 import App from '../App'
+import Spinner from '../components/atoms/spinner/Spinner'
 import { AuthStateContext } from '../context/auth-context'
 import DashboardRoutesPublic from './DashboardRoutesPublic'
 import PrivateRoute from './PrivateRoute'
 import PublicRoute from './PublicRoute'
 
 const AppRouter = () => {
+  const [loadingToken, setLoadingToken] = React.useState(false)
   const { dataUser, setDataUserFromDB } = useContext(AuthStateContext)
 
-  React.useEffect(() => {
-    const token = localStorage.getItem('token')
-    if (!token) return
-    setDataUserFromDB({
-      dataUser: {
-        username: "thiago",
-        email: "test@tes.com"
-      },
-      isAuthenticated: true
+  const token = localStorage.getItem('token')
 
-    })
+  React.useEffect(() => {
+    if (!token) {
+      return
+    }
+
+    setLoadingToken(true)
+    console.log('get token')
 
     fetch(`http://127.0.0.1:1820/api/auth/verify-user/${token}`)
       .then(resp => resp.json())
       .then(data => {
+
+        if (data.message) {
+          console.log(data.message)
+          setLoadingToken(false)
+          return
+        }
         setDataUserFromDB(data)
+        setLoadingToken(false)
+
       })
       .catch(error => {
+        setLoadingToken(false)
         console.log(error)
       })
 
-    console.log(token)
+    //eslint-disable-next-line
   }, [])
+
+  if (loadingToken) {
+    return <Spinner />
+  }
 
 
   return (
