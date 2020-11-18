e20fc8fa7cd87760a8aec0f5721521da44f5baeb
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-11-18 09:10:43 -0600

    Refactor: AlertMessage component, new feature

--- src/components/loginScreen/alerts/alert-message/AlertMessage.js
+++ src/components/loginScreen/alerts/alert-message/AlertMessage.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
-const AlertMessage = ({message, type, color}) => {
+const AlertMessage = ({ message, type, color }) => {
+  const [hidden, setHidden] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHidden(true)
+    }, 3000)
+    return () => {
+      setHidden(false)
+    }
+  }, [message])
+
+  if (hidden) return null
+
   return (
     <div
       className={`bg-${color}-100 border ml-4 mb-10 border-${color}-400 text-${color}-700 px-4 py-3 rounded relative`}
